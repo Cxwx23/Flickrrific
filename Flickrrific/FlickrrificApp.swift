3dff9b35c62399ec7686f365f69da11a62c7c13c
Cole Warner <cxwx23@gmail.com>
2024-09-25 01:12:40 -0400

    Initial Commit

---
+++ Flickrrific/FlickrrificApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrrificApp.swift
+//  Flickrrific
+//
+//  Created by Cole Warner on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrrificApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
